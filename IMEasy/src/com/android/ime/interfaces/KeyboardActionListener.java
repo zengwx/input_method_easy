aa178c48be99b46d9c2c98f47a9f178a508b9bd5
hailongqiu <hailongqiu@xgimi.com>
2015-06-20 12:01:20 +0800

    fd

--- IMEasy/src/com/android/ime/interfaces/KeyboardActionListener.java
+++
@@ -1,9 +0,0 @@
-package com.android.ime.interfaces;
-
-public interface KeyboardActionListener {
-
-	public void onCodeInput(int primaryCode, int x, int y);
-
-	public void onTextInput(CharSequence text);
-	
-}
--- IMEasy/src/com/android/ime/keyboard/KeyBoardParse.java
+++
@@ -1,5 +0,0 @@
-package com.android.ime.keyboard;
-
-public class KeyBoardParse {
-
-}
