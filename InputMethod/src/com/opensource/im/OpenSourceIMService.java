11e565882ce67921855ff66814cab47c9c3b1a6e
hailong.qiu <hailong.qiu@xgimi.com>
2015-06-22 13:46:19 +0800

    fdfdf

--- InputMethod/src/com/opensource/im/OpenSourceIMService.java
+++ InputMethod/src/com/opensource/im/OpenSourceIMService.java
@@ -2,13 +2,16 @@ package com.opensource.im;
 
 import android.inputmethodservice.InputMethodService;
 import android.text.InputType;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
+import android.webkit.WebView.FindListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class OpenSourceIMService extends InputMethodService {
 
@@ -17,7 +20,7 @@ public class OpenSourceIMService extends InputMethodService {
 	private View mCandView;
 	private View number_lay;
 	private View defualt_lay;
-	
+
 	@Override
 	public View onCreateInputView() {
 		LayoutInflater inflater = getLayoutInflater();
@@ -25,23 +28,30 @@ public class OpenSourceIMService extends InputMethodService {
 		number_lay = mRootView.findViewById(R.id.number_lay);
 		defualt_lay = mRootView.findViewById(R.id.defualt_lay);
 		//
-		Button button1= (Button) mRootView.findViewById(R.id.button1);
+		Button button1 = (Button) mRootView.findViewById(R.id.button1);
 		button1.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				onTextInput("1");
 			}
 		});
-		Button button2= (Button) mRootView.findViewById(R.id.button2);
+		Button button2 = (Button) mRootView.findViewById(R.id.button2);
 		button2.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				onCodeInput(KeyBoardID.CODE_ACTION_NEXT);
 			}
 		});
+		Button button3 = (Button) mRootView.findViewById(R.id.button3);
+		button3.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onTextInput("2");
+			}
+		});
 		return mRootView;
 	}
-
+	
 	@Override
 	public View onCreateCandidatesView() {
 		LayoutInflater inflater = getLayoutInflater();
@@ -52,7 +62,7 @@ public class OpenSourceIMService extends InputMethodService {
 	@Override
 	public void onStartInputView(EditorInfo editorInfo, boolean restarting) {
 		mConnection = getCurrentInputConnection();
-		setCandidatesViewShown(true);
+		setCandidatesViewShown(true); // 候选框.
 		getKeyboardMode(editorInfo);
 		super.onStartInputView(editorInfo, restarting);
 	}
@@ -122,36 +132,50 @@ public class OpenSourceIMService extends InputMethodService {
 
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if (true)
-			return false;
-		switch (keyCode) {
-		case KeyEvent.KEYCODE_NUMPAD_0:
-		case KeyEvent.KEYCODE_NUMPAD_1:
-		case KeyEvent.KEYCODE_NUMPAD_2:
-		case KeyEvent.KEYCODE_NUMPAD_3:
-		case KeyEvent.KEYCODE_NUMPAD_4:
-		case KeyEvent.KEYCODE_NUMPAD_5:
-		case KeyEvent.KEYCODE_NUMPAD_6:
-		case KeyEvent.KEYCODE_NUMPAD_7:
-		case KeyEvent.KEYCODE_NUMPAD_8:
-		case KeyEvent.KEYCODE_NUMPAD_9:
-			if (event.isNumLockOn()) {
-				onTextInput(Integer.toString(keyCode
-						- KeyEvent.KEYCODE_NUMPAD_0));
-			}
+		if (!isInputViewShown()) {
+			return super.onKeyDown(keyCode, event);
+		}
+		if (processKey(event, false)) {
 			return true;
-
-		default:
-			break;
 		}
 		return super.onKeyDown(keyCode, event);
 	}
 
 	@Override
 	public boolean onKeyUp(int keyCode, KeyEvent event) {
-		if (true)
-			return false;
+		if (!isInputViewShown()) {
+			return super.onKeyDown(keyCode, event);
+		}
+		if (processKey(event, true)) {
+			return true;
+		}
 		return super.onKeyUp(keyCode, event);
+	}
+
+	public boolean processKey(KeyEvent event, boolean realAction) {
+		int keyCode = event.getKeyCode();
+		// 按返回键.
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			return false;
+		} 
+		switch (keyCode) {
+		case KeyEvent.KEYCODE_BACK:
+			return false;
+		case KeyEvent.KEYCODE_DPAD_DOWN:
+		case KeyEvent.KEYCODE_DPAD_RIGHT:
+		case KeyEvent.KEYCODE_DPAD_LEFT:
+		case KeyEvent.KEYCODE_DPAD_UP:
+			//
+			if (realAction)
+				mRootView.onKeyDown(keyCode, event);
+			else
+				mRootView.onKeyUp(keyCode, event);
+			return true;
+		default:
+			break;
+		}
+		
+		return true;
 	}
 
 	public void onTextInput(CharSequence text) {
@@ -167,6 +191,7 @@ public class OpenSourceIMService extends InputMethodService {
 			break;
 		// IME_ACTION_NEXT: next操作，将用户带入到该文本框的写一个输入框中。
 		case KeyBoardID.CODE_ACTION_NEXT:
+			// EditorInfo.IME_ACTION_GO
 			mConnection.performEditorAction(EditorInfo.IME_ACTION_NEXT);
 			break;
 		case KeyBoardID.CODE_ACTION_ENTER:
